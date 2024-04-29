f7e0d0f69a33d5d91dd3667cf83b07d0e1a07dfe
Alex Litvinov <me@litvinov.co.nz>
2024-04-30 01:34:17 +0400

    working on it

--- subscribe-to-landscape.sh
+++ subscribe-to-landscape.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Access the variable value passed from the Python script
-account-name=$1
-
-echo "Subscribing with account-name : $account-name"
-
 sudo apt-get update 
 sudo apt-get install landscape-client -y
-sudo landscape-config --account-name="$(account-name)" --computer-title="$(hostname)" --registration-key= --silent
+sudo landscape-config --account-name="$1" --computer-title="$(hostname)" --registration-key= --silent
